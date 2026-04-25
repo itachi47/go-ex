081ab34d9304624e0595678e68e6b2364ea64dc1
Kuldeep Singh Karhana <kuldeepkarhana21@gmail.com>
2026-04-25 23:37:19 +0530

    feat: bubble sort

---
+++ algorithms/sort/bubble/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var t int
+	fmt.Fscan(in, &t)
+
+	for ; t > 0; t-- {
+		var n int
+		fmt.Fscan(in, &n)
+
+		arr := make([]int, n)
+
+		for i := 0; i < n; i++ {
+			fmt.Fscan(in, &arr[i])
+		}
+
+		BubbleSort(arr)
+		for i := 0; i < n; i++ {
+			if i > 0 {
+				fmt.Fprint(out, " ")
+			}
+			fmt.Fprint(out, arr[i])
+		}
+		fmt.Fprintln(out)
+	}
+}

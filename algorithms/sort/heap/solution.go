ccfc102140240e96cb02d88f3f1a9220819061a3
Kuldeep Singh Karhana <kuldeepkarhana21@gmail.com>
2026-04-30 16:01:03 +0530

    feat: heap sort

---
+++ algorithms/sort/heap/solution.go
@@ -0,0 +1,34 @@
+package main
+
+func heapify(arr []int, n, i int) {
+	largest := i
+
+	left := 2*i + 1
+	right := 2*i + 2
+
+	if left < n && arr[largest] < arr[left] {
+		largest = left
+	}
+
+	if right < n && arr[largest] < arr[right] {
+		largest = right
+	}
+
+	if largest != i {
+		arr[i], arr[largest] = arr[largest], arr[i]
+		heapify(arr, n, largest)
+	}
+}
+
+func HeapSort(arr []int, n int) {
+
+	for i := n/2 - 1; i >= 0; i-- {
+		heapify(arr, n, i)
+	}
+
+	for i := n - 1; i > 0; i-- {
+		arr[0], arr[i] = arr[i], arr[0]
+		heapify(arr, i, 0)
+	}
+
+}
---
+++ algorithms/sort/heap/solution_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestBubbleSort(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []int
+		want  []int
+	}{
+		{
+			name:  "basic unsorted",
+			input: []int{5, 1, 9, 2, 7},
+			want:  []int{1, 2, 5, 7, 9},
+		},
+		{
+			name:  "already sorted",
+			input: []int{1, 2, 3, 4, 5},
+			want:  []int{1, 2, 3, 4, 5},
+		},
+		{
+			name:  "reverse sorted",
+			input: []int{5, 4, 3, 2, 1},
+			want:  []int{1, 2, 3, 4, 5},
+		},
+		{
+			name:  "duplicates",
+			input: []int{4, 2, 4, 1, 2, 3},
+			want:  []int{1, 2, 2, 3, 4, 4},
+		},
+		{
+			name:  "negative numbers",
+			input: []int{-1, 3, 0, -5, 2},
+			want:  []int{-5, -1, 0, 2, 3},
+		},
+		{
+			name:  "single element",
+			input: []int{42},
+			want:  []int{42},
+		},
+		// {
+		// 	name:  "empty slice",
+		// 	input: []int{},
+		// 	want:  []int{},
+		// },
+	}
+
+	for _, tt := range tests {
+		got := append([]int(nil), tt.input...) // copy input
+
+		HeapSort(got, len(got))
+
+		if !reflect.DeepEqual(got, tt.want) {
+			t.Fatalf("%s: got %v want %v", tt.name, got, tt.want)
+		}
+	}
+}
